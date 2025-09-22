97ebb8927da2f94945e94a08a4a023e83f5edbad
oliver <oliversalvesen@proton.me>
2025-09-22 10:44:37 +0200

    fix zen settings

--- modules/home-manager/programs/browsers/zen-browser.nix
+++ modules/home-manager/programs/browsers/zen-browser.nix
@@ -15,59 +15,36 @@
       };
       isDefault = true;
       settings = {
-        "zen" = {
-          "welcome-screen.seen" = true;
-          "tab-unloader.timeout-minutes" = 20;
-          "widget.linux.transparency" = true;
-        };
-        "extensions" = {
-          "webextensions.ExtensionStorageIDB.migrated.search@kagi.com" = true;
-          "autoDisableScopes" = 0;
-        };
+        "zen.welcome-screen.seen" = true;
+        "zen.tab-unloader.timeout-minutes" = 20;
+        "zen.widget.linux.transparency" = true;
+        "extension.webextensions.ExtensionStorageIDB.migrated.search@kagi.com" = true;
+        "extension.autoDisableScopes" = 0;
         "ui.key.menuAccessKeyFocuses" = false;
-        "browser" = {
-          "firefox-view.feature-tour" = "{'screen':'','complete':true}";
-          "bookmarks.addedImportButton" = false;
-          "download.useDownloadDir" = false;
-          "startup.homepage" = "kagi.com";
-          "newtabpage.activity-stream." = {
-            "telemetry" = false;
-            "feeds" = {
-              "telemetry" = false;
-              "topsites" = false;
-            };
-          };
-          "urlbar" = {
-            #"placeholderName" = "search for something man";
-            "suggest.quicksuggest" = {
-              "sponsored" = false;
-              "nonsponsored" = false;
-            };
-            "trending.featureGate" = false;
-          };
-        };
-        "datareporting" = {
-          "policy.dataSubmissionEnabled" = false;
-          "healthreport.uploadEnabled" = false;
-        };
-        "toolkit" = {
-          "telemetry" = {
-            "unified" = false;
-            "enabled" = false;
-            "server" = "data:,";
-            "archive.enabled" = false;
-            "newProfilePing.enabled" = false;
-            "shutdownPingSender.enabled" = false;
-            "updatePing.enabled" = false;
-            "bhrPing.enabled" = false;
-            "firstShutdownPing.enabled" = false;
-            "coverage.opt-out" = true;
-          };
-          "coverage" = {
-            "opt-out" = true;
-            "endpoint.base" = "";
-          };
-        };
+        "browser.firefox-view.feature-tour" = "{'screen':'','complete':true}";
+        "browser.bookmarks.addedImportButton" = false;
+        "browser.download.useDownloadDir" = false;
+        "browser.startup.homepage" = "kagi.com";
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+        "browser.urlbar.trending.featureGate" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "data:,";
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.coverage.endpoint.base" = "";
       };
       search = {
         default = "Kagi";
