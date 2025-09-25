3df07ed2e7e54d9b6c274582196f58c19547dd05
oliver <oliversalvesen@proton.me>
2025-09-25 11:31:47 +0200

    add settings file for firefox based browsers

---
+++ modules/home-manager/programs/browsers/firefox/settings.nix
@@ -0,0 +1,31 @@
+{
+  settings = {
+    "extension.webextensions.ExtensionStorageIDB.migrated.search@kagi.com" = true;
+    "extension.autoDisableScopes" = 0;
+    "ui.key.menuAccessKeyFocuses" = false;
+    "browser.firefox-view.feature-tour" = "{'screen':'','complete':true}";
+    "browser.bookmarks.addedImportButton" = false;
+    "browser.download.useDownloadDir" = false;
+    "browser.startup.homepage" = "kagi.com";
+    "browser.newtabpage.activity-stream.telemetry" = false;
+    "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+    "browser.newtabpage.activity-stream.feeds.topsites" = false;
+    "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+    "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+    "browser.urlbar.trending.featureGate" = false;
+    "datareporting.policy.dataSubmissionEnabled" = false;
+    "datareporting.healthreport.uploadEnabled" = false;
+    "toolkit.telemetry.unified" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.server" = "data:,";
+    "toolkit.telemetry.archive.enabled" = false;
+    "toolkit.telemetry.newProfilePing.enabled" = false;
+    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+    "toolkit.telemetry.updatePing.enabled" = false;
+    "toolkit.telemetry.bhrPing.enabled" = false;
+    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+    "toolkit.telemetry.coverage.opt-out" = true;
+    "toolkit.coverage.opt-out" = true;
+    "toolkit.coverage.endpoint.base" = "";
+  };
+}
