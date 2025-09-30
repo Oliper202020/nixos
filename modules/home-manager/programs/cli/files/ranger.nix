432bf97b61753b81af6d0fac2f0c55ebb68ff110
oliver <oliversalvesen@proton.me>
2025-09-30 12:44:19 +0200

    ranger yneo remap

--- modules/home-manager/programs/cli/files/ranger.nix
+++ modules/home-manager/programs/cli/files/ranger.nix
@@ -1,6 +1,29 @@
 {
   programs.ranger = {
     enable = true;
+    mappings = {
+      y = "move left=1";
+      n = "move down=1";
+      e = "move up=1";
+      o = "move right=1";
+
+      Y = "history_go -1";
+      N = "move down=0.5 pages=True";
+      E = "move up=0.5 pages=True";
+      O = "history_go 1";
+
+      #y = "pager_move left=4";
+      #n = "pager_move down=1";
+      #e = "pager_move up=1";
+      #o = "pager_move right=4";
+
+  #    y = "taskview_move left=4";
+   #   n = "taskview_move down=1";
+    #  e = "taskview_move up=1";
+     # o = "taskview_move right=4";
+
+
+    };
     # settings = {
     #preview_images = true;
     #  preview_images_method = "kitty";
