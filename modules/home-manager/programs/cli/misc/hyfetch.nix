7e98c9b3f80ca1ac7aa290eccd28a2661fab0c43
oliver <oliversalvesen@proton.me>
2025-09-30 12:33:23 +0200

    fix hyfetch

--- modules/home-manager/programs/cli/misc/hyfetch.nix
+++ modules/home-manager/programs/cli/misc/hyfetch.nix
@@ -2,13 +2,14 @@
   programs.hyfetch = {
     enable = true;
     settings = {
-      preset = "random";
+      preset = "rainbow";
       mode = "rgb";
       backend = "fastfetch";
       light = "0.57";
       color_align = {
         mode = "horizontal";
       };
+      pride_month_disable = false;
     };
   };
 }
