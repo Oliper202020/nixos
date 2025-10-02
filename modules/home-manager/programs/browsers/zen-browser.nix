a8182d370504d6914059a6002343a5cdcbba9427
oliver <oliversalvesen@proton.me>
2025-10-02 12:11:57 +0200

    add workspace config for zen browser

--- modules/home-manager/programs/browsers/zen-browser.nix
+++ modules/home-manager/programs/browsers/zen-browser.nix
@@ -1,6 +1,7 @@
-{pkgs, inputs, ...}: let
+{pkgs, inputs, config, ...}: let
   settings = import ./firefox/settings.nix;
-  extensions = import ./firefox/extensions.nix{ inherit pkgs; };
+  containers = import ./firefox/containers.nix;
+  extensions = import ./firefox/extensions.nix { inherit pkgs; };
 in {
   imports = [
     inputs.zen-browser.homeModules.twilight
@@ -12,11 +13,29 @@ in {
       NoDefaultBookmarks = true;
       DisableProfileImport = true;
       DontCheckDefaultBrowser = true;
+      ExtensionSettings."*".installation_mode = "force_installed";
     };
     profiles.oliver = {
       bookmarks = {
       };
       isDefault = true;
+      spacesForce = true;
+      spaces = let
+        containers = config.programs.zen-browser.profiles.oliver.containers;
+      in {
+        "Personal" = {
+          id = "c6de089c-410d-4206-961d-ab11f988d40a";
+          icon = "https://raw.githubusercontent.com/lucide-icons/lucide/f4d451de802c7110ad7acd14f2d76746bde3ebfe/icons/house.svg";
+          container = containers."Personal".id;
+          position = 1000;
+        };
+        "School" = {
+          id = "cdd10fab-4fc5-494b-9041-325e5759195b";
+          icon = "https://raw.githubusercontent.com/lucide-icons/lucide/refs/heads/main/icons/school.svg";
+          container = containers."School".id;
+          position = 2000;
+        };
+      };
       settings = {
         "zen.welcome-screen.seen" = true;
         "zen.tab-unloader.timeout-minutes" = 20;
@@ -49,8 +68,7 @@ in {
         force = true; # this is required because otherwise the search.json.mozlz4 symlink gets replaced on every firefox restart
       };
       extensions = {
-
       } // extensions;
-    }; #// containers;
+    } // containers;
   };
 }
