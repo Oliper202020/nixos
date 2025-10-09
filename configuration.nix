b5b89043eebe0e9108834e98b625277f9ce19112
oliver <oliversalvesen@proton.me>
2025-10-09 10:12:44 +0200

    add anyrun as trusted public key

--- configuration.nix
+++ configuration.nix
@@ -57,8 +57,12 @@
   nix = {
     channel.enable = true;
     settings = {
-      substituters = [""];
-      trusted-public-keys = [""];
+      extra-substituters = [
+        "https://anyrun.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      ];
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
     };
