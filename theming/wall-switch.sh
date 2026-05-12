c069134f824f12cb2ff1d5fae9f0336106da7694
oliver <oliversalvesen@proton.me>
2026-05-12 12:49:02 +0200

    delete unused files

--- theming/wall-switch.sh
+++
@@ -1,5 +0,0 @@
-DIR=$HOME/.dotfiles/theming/walls/
-PICS=($(ls ${DIR}))
-
-RANDOMPICS=${PICS[$RANDOM % ${#PICS[@]} ]}
-swww img ${DIR}/${RANDOMPICS} --transition-duration 2 --transition-fps 30 --transition-type any
