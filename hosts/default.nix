53876a2130e0ca2faadd5aa1252d4c78ff81c575
oliver <oliversalvesen@proton.me>
2026-05-12 12:50:20 +0200

    Update default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,9 @@
 {
-  #allHardware = import ./allHardware;
+  imports = [
+    ./common
+  ];
   hp-victus = import ./hp-victus;
-  asus-vivobook = import ./asus-vivobook;
+  framework = import ./framework;
   hpe-dl380p-gen8 = import ./hpe-dl380p-gen8;
   thinkpad-l540 = import ./thinkpad-l540;
   #common = import ./common;
