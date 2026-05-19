e9972cb7fe97f8ae2697b92b0a3d4d52ec5a0eef
oliver <oliversalvesen@proton.me>
2026-05-19 12:27:33 +0200

    update apple color emoji

--- pkgs/apple-color-emoji.nix
+++ pkgs/apple-color-emoji.nix
@@ -1,11 +1,27 @@
-{ lib }:
-let
+{
+  lib,
+  stdenv,
+}:
+stdenv.mkDerivation rec {
   pname = "apple-color-emoji";
-  version = "55ef43bbeca8adfedc2f5db833ae85e8266b6157";
-in
-builtins.fetchurl {
-  name = "${pname}-${version}";
-  url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/ios-14.6/AppleColorEmoji.ttf";
-  # sha256 = lib.fakeSha256;
-  sha256 = "sha256:1hi9cgbgl7g728plm6lfqywmva0bn90wjjv7jmnnbwz1sfqq7c5i";
+  version = "v18.4";
+
+  src = builtins.fetchurl {
+    name = "${pname}-${version}";
+    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v18.4/AppleColorEmoji.ttf";
+    sha256 = "sha256:1ggahpw54rjpxirjbyarwd5gvvg1hi08zw4c1nab8dqls5xhgzd4";
+  };
+
+  # No build steps are needed – just copy the font into $out.
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    cp $src $out/share/fonts/truetype/AppleColorEmoji.ttf
+  '';
+
+  meta = with lib; {
+    description = "Apple Color Emoji font for Linux";
+    homepage = "https://github.com/samuelngs/apple-emoji-linux";
+    # license = licenses.unfree; # the font’s license
+    platforms = platforms.all;
+  };
 }
