d19b171286532ccda49a0eab58d4a47c755a5c36
oliver <oliversalvesen@proton.me>
2026-05-19 12:25:43 +0200

    Update config.nix

--- modules/home-manager/config.nix
+++ modules/home-manager/config.nix
@@ -1,14 +1,13 @@
 {
   # home = {
-    # username = "oliver";
-    # homeDirectory = "/home/oliver";
+  # username = "oliver";
+  # homeDirectory = "/home/oliver";
   # };
-
   programs.git = {
     enable = true;
-    userName = "oliver";
-    userEmail = "oliversalvesen@proton.me";
-    extraConfig = {
+    settings.user = {
+      name = "oliver";
+      email = "oliversalvesen@proton.me";
       init.defaultBranch = "main";
     };
   };
@@ -20,13 +19,13 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
-
+  home.stateVersion = "26.05"; # Please read the comment before changing.
   home.sessionVariables = {
-    EDITOR = "zed";
+    # EDITOR = "zed";
     BROWSER = "zen-twilight";
-    TERMINAL = "kitty";
+    # TERMINAL = "kitty";
   };
+  # https://github.com/rose-pine/steam-os/
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
