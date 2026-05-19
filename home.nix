cdd037c301f276907f44e6fd37c7ab72f820ebf4
oliver <oliversalvesen@proton.me>
2026-05-19 12:25:04 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
     inputs.home-manager.nixosModules.home-manager
   ];
   home-manager = {
+    backupFileExtension
     users.oliver = import ./modules/home-manager;
     extraSpecialArgs = { inherit inputs settings pkgs; };
   };
